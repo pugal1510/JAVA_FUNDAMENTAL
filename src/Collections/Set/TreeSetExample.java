4edfe4352a9bd711604c2dc98164423a50c8e918
pugal1510 <108538519+pugal1510@users.noreply.github.com>
2025-12-17 23:26:23 +0530

    updating the streams

--- src/Collections/Set/TreeSetExample.java
+++ src/Collections/Set/TreeSetExample.java
@@ -5,7 +5,7 @@ import java.util.TreeSet;
 public class TreeSetExample {
     public static void main(String[] args) {
 
-        //i will sort the give
+        //it will sort the give
         //it will not allow the duplicate
         //it will not allow the hetrogenous
 //        it will not allow the null evem 1 null also it wont allow
